db23cf4b17082ae54fcf99d735419ca07abcb124
Leandro Segovia <ldlsegovia@gmail.com>
2016-04-08 14:28:13 -0300

    feat(models.JobNotifier): add method to find jobs by identifier

--- app/models/job_notifier/job.rb
+++ app/models/job_notifier/job.rb
@@ -2,13 +2,17 @@ module JobNotifier
   class Job < ActiveRecord::Base
     attr_accessor :decoded_identifier
 
-    before_save :encode_identifier
+    before_save do
+      self.identifier = self.class.encode_identifier(decoded_identifier) if decoded_identifier
+    end
 
-    private
+    def self.job_by_identifier(decoded_identifier)
+      encoded_job_identifier = encode_identifier(decoded_identifier)
+      JobNotifier::Job.where(identifier: encoded_job_identifier).first
+    end
 
-    def encode_identifier
-      return unless decoded_identifier
-      self.identifier = Digest::MD5.hexdigest(decoded_identifier.to_s)
+    def self.encode_identifier(identifier)
+      Digest::MD5.hexdigest(identifier.to_s)
     end
   end
 end
