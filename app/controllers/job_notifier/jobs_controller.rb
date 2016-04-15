746f7d5c22af1b2d5040c57dcd96c55d214c2e43
Leandro Segovia <ldlsegovia@gmail.com>
2016-04-15 18:17:23 -0300

    feat(JobsController): add skip_before_action :verify_authenticity_token

--- app/controllers/job_notifier/jobs_controller.rb
+++ app/controllers/job_notifier/jobs_controller.rb
@@ -1,5 +1,7 @@
 module JobNotifier
   class JobsController < ActionController::Base
+    skip_before_action :verify_authenticity_token
+
     def index
       jobs = Job.unnotified_by_identifier!(params[:identifier])
       render json: jobs
